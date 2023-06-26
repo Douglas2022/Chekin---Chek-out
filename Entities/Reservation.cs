1a6b4c8b4f1579439cdc1f21fc0dcf5b435892cd
Douglas Nunes <paulodnunes2016@gmail.com>
2023-06-25 21:09:58 -0300

    Chekin - Chek out

---
+++ Entities/Reservation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1.Entities
+{
+    public class Reservation
+    {
+        public int RoomNunber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime CheckOut { get; set; }
+       
+        public Reservation()
+        {
+        }
+        public Reservation(int roomNunber, DateTime checkin, DateTime checkOut)
+        {
+            RoomNunber = roomNunber;
+            Checkin = checkin;
+            CheckOut = checkOut;
+        }
+        public int Duration()
+        {
+            TimeSpan durantion = CheckOut.Subtract(Checkin);
+            return (int)durantion.TotalDays;
+        }
+        public override string ToString()
+        {
+            return " Room "
+                + RoomNunber
+                + " , Checkin "
+                + Checkin.ToString(" dd/mm/yyyy: " )
+                + " , Chekout "
+                + CheckOut.ToString(" dd/mm/yyyy: " )
+                + " , "
+                + Duration()
+                + " Night";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Entities;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Room Number: " );
+            int Number = int.Parse( Console.ReadLine() );
+            Console.Write("  Checkin date ( dd/mm/yyyy ) : ");
+            DateTime chekin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check Out ( dd/mm/yyyy ) ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            if( checkout <= chekin)
+            {
+                Console.WriteLine(" Error in reservation; Check - out date must be after checkin  date ");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(Number, chekin, checkout);
+                Console.WriteLine("Reservation " + reservation);
+            }
+
+           
+
+        }
+    }
+}
